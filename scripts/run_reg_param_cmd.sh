7d759901fed526a134c34a07f5d273271b31c72d
nikhil153 <nikhil153@gmail.com>
2018-09-25 14:27:58 -0400

    changes to set local_env baesd on local/cluster system

--- scripts/run_reg_param_cmd.sh
+++ scripts/run_reg_param_cmd.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #source /opt/minc/1.9.16/minc-toolkit-config.sh
-source /ipl/quarantine/experimental/2013-02-15/init.sh
+#source /ipl/quarantine/experimental/2013-02-15/init.sh
+source $1
 tmpfile=$(mktemp /tmp/xfm_invert.XXXXXX)
-xfminvert $1 $tmpfile -clobber
+xfminvert $2 $tmpfile -clobber
 reg_param=$(xfm2param $tmpfile)
 rm -f $tmpfile
 echo $reg_param
--- scripts/run_xcorr_cmd.sh
+++ scripts/run_xcorr_cmd.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #source /opt/minc/1.9.16/minc-toolkit-config.sh
-source /ipl/quarantine/experimental/2013-02-15/init.sh
-xcorr=`xcorr_vol $1 $2`
+#source /ipl/quarantine/experimental/2013-02-15/init.sh
+source $1
+xcorr=$(xcorr_vol $2 $3)
 echo $xcorr
